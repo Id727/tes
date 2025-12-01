4854170d3cea32a28ca8e2ed2bc0e615c0203e62
Id727 <rian012529@gmail.com>
2025-12-01 10:50:34 +0800

    Create product-data.js

---
+++ product-data.js
@@ -0,0 +1,22 @@
+// product-data.js
+
+// Data Produk
+const products = {
+    indosat: [
+        { name: 'Indosat 50GB', price: 55000, desc: '✅ Ready Stock | - Kuota Indosat Aktif 28 Hari<br>- Jaringan 4G/5G seluruh Indonesia<br>- Garansi sesuai masa aktif<br>- Proses cepat otomatis' },
+        { name: 'Indosat 80GB', price: 95000, desc: '✅ Ready Stock | - Kuota Indosat Aktif 30 Hari<br>- Jaringan 4G/5G seluruh Indonesia<br>- Garansi sesuai masa aktif<br>- Proses cepat otomatis' }
+    ],
+    telkomsel: [
+        { name: 'Telkomsel 30GB', price: 65000, desc: '✅ Ready Stock | - Kuota Nasional aktif 30 hari<br>- Berlaku semua jaringan<br>- Garansi masa aktif penuh<br>- Aktivasi instan' },
+        { name: 'Telkomsel 80GB', price: 120000, desc: '✅ Ready Stock | - Kuota Nasional aktif 30 hari<br>- Bonus tambahan 5GB YouTube<br>- Garansi masa aktif penuh<br>- Aktivasi instan' }
+    ],
+    xl: [
+        { name: ' SUPERMINI', price: 39000, desc: '❌ Habis | Masa aktif: 27/28 hari<br>- AREA 1: 13–15 GB<br>- AREA 2: 15–17 GB<br>- AREA 3: 20–22 GB<br>- AREA 4: 30–32 GB' },
+        { name: ' MINI', price: 50000, desc: '✅ Ready Stock | Masa aktif: 27/28 hari<br>- AREA 1: 32–34 GB<br>- AREA 2: 34–36 GB<br>- AREA 3: 39–41 GB<br>- AREA 4: 49–51 GB' },
+        { name: ' BIG', price: 55000, desc: '❌ Habis | Masa aktif: 27/28 hari<br>- AREA 1: 38–40 GB<br>- AREA 2: 40–42 GB<br>- AREA 3: 45–47 GB<br>- - AREA 4: 55–57 GB' }, 
+        { name: ' JUMBO V2', price: 63000, desc: '✅ Ready Stock | Masa aktif: 27/28 hari<br>- AREA 1: 50–53 GB<br>- AREA 2: 52–55 GB<br>- AREA 3: 57–60 GB<br>- AREA 4: 67–70 GB' },
+        { name: ' BIG SUPER', price: 71000, desc: '✅ Ready Stock | Masa aktif: 27/28 hari<br>- AREA 1: 63–65 GB<br>- AREA 2: 65–67 GB<br>- AREA 3: 70–72 GB<br>- AREA 4: 80–82 GB' },
+        { name: ' JUMBO', price: 83000, desc: '✅ Ready Stock | Masa aktif: 27/28 hari<br>- AREA 1: 65 GB<br>- AREA 2: 70 GB<br>- AREA 3: 83 GB<br>- AREA 4: 123 GB' },
+        { name: ' JUMBO V3', price: 88000, desc: '✅ Ready Stock | Masa aktif: 27/28 hari<br>- AREA 1: 87–90 GB<br>- AREA 2: 89–92 GB<br>- AREA 3: 94–97 GB<br>- AREA 4: 104–107 GB' }
+    ]
+};
